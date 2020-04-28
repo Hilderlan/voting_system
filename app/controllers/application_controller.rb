6449d417e9893d0352e469507d3097bcd2152f39
Hilderlan <hilderlan@infoway-pi.com.br>
2020-04-28 17:59:45 -0300

    Campaign controllers defined

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Ensure that the user authenticates to the system
+  before_action :authenticate_user!
+ 
+  protected
+  
+  # Allow adding the name attribute using the devise sign_up method
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
